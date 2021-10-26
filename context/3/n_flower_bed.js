85898f9a6540de52aa55b8560b9360a3b55667c8
Ivan Pischita <aaib@tut.by>
2021-10-26 22:00:52 +0300

    Правильное решение

--- context/3/n_flower_bed.js
+++ context/3/n_flower_bed.js
@@ -21,64 +21,57 @@ function solve() {
 
     const result = [];
 
-    const skipLines = 1;
-    let resultRanges = [];
-    for (let i = 0; i < countGardeners; i++) {
-        let range = inputLines[i + skipLines].split(' ');
-        let left = Number(range[0]);
-        let right = Number(range[1]);
-
-        let newRange = {
-            left: left,
-            right: right
-        };
-        for (let resIndex = 0; resIndex < resultRanges.length; resIndex++){
-            let item = resultRanges[resIndex];
-            
-            if ( (left >= item.left && left <= item.right)
-                    || (right >= item.left && right < item.right)
-                    || (left <= item.left && right >= item.right) ) {
-
-                newRange.left = Math.min(item.left, newRange.left);
-                newRange.right = Math.max(item.right, newRange.right);
-             
-                resultRanges.splice(resIndex, 1);
-                resIndex--;
-            }
-        }
-
-        resultRanges.push(newRange);        
-    }
-
-    resultRanges = resultRanges.sort((a, b) => {
-        if (a.left < b.left) {
-            return -1;
-        } else if (b.left < a.left) {
-            return 1
-        } else {
-            return 0;
+    inputLines.splice(0, 1);
+    let ranges = inputLines.map(item => {
+        let str = item.split(' ');
+        return {
+            left: Number(str[0]),
+            right: Number(str[1]),
         }
     });
 
-    strOld = '';
-    for (let range of resultRanges) {
-        let str = `${range.left} ${range.right}`;
-        if(strOld != str){
-            console.log(str);
+    ranges = ranges.sort((a, b)=>{
+        if(a.left < b.left){
+            return -1;
+        }else if(b.left < a.left){
+            return 1;
+        }else{
+            return 0;
         }
 
-        strOld = str;
+    });
+
+    let resultRanges = [ranges[0]];
+    let currentResultIndex = 0;
+    for(let range of ranges){
+        
+        let item = resultRanges[currentResultIndex];
+
+        if ( (range.left >= item.left && range.left <= item.right)
+                    || (range.right >= item.left && range.right < item.right) ) {
+
+            if(item.right < range.right){
+               item.right = range.right;
+            }
+                        
+        }else{
+            resultRanges.push(range);
+            currentResultIndex++;
+        }
     }
+    
+    
+    resultRanges.forEach(item =>{
+        console.log(`${item.left} ${item.right}`);
+    });
 }
 
 
-let input = `6
-1 3
-3 5
-4 6
-5 6
-2 4
-7 10`;
+let input = `4
+7 8
+7 8
+2 3
+6 10`;
 
 inputLines = input.split('\n');
 
