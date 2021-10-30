e778fdfad0f081fca3244c5f1bb3ad4ea722c976
Ivan Pischita <aaib@tut.by>
2021-10-30 17:18:17 +0300

    Правильное решение

--- sprint_3/a_search.js
+++ sprint_3/a_search.js
@@ -1,74 +1,91 @@
-function searchMinElement(arr, firstElement, miminum, left, right){
-    if((right - left) === 1){
-        return left;
-    }
+function search(arr, firstElement, searchElement, left, right){
+    let middle = Math.floor((left + right) / 2 );  
 
-    let middle = Math.floor((left + right) / 2 );
-    
-    if(arr[middle] > firstElement){
-        return searchMinElement(arr, firstElement, miminum, middle, right);
-    }else{
-        if(arr[middle] < miminum){
-            return searchMinElement(arr, firstElement, arr[middle], left, middle+1);    
-        }else{
-            return searchMinElement(arr, firstElement, miminum, middle, right);
-        }   
-    }
-}
-
-function search(arr, k, left, right){
-    if(right <= left){
-        return -1;
-    }
-
-    let middle = Math.floor( (left + right) / 2 );
-
-    if(arr[middle] === k){
+    if(arr[middle] === searchElement){
         return middle;
     }
 
-    if( k < arr[middle]){
-        return search(arr, k, left, middle);
-    }else{
-        return search(arr, k, middle + 1, right);
+    if(arr[left] === searchElement){
+        return left;
     }
+
+    if(arr[right] === searchElement){
+        return right;
+    }
+
+    if(right - left <= 0 ){
+        return -1;
+    }
+
+     let isBreakingLeft = arr[left] > arr[middle];
+
+    if(searchElement > arr[left] && searchElement < arr[middle]){
+        return search(arr, firstElement, searchElement, left, middle);        
+    }else{ 
+        if(isBreakingLeft && arr[middle] < searchElement && arr[left] < searchElement){
+           return search(arr, firstElement, searchElement, left, middle);
+        }else if(isBreakingLeft && searchElement < arr[middle] && searchElement < arr[left]){
+            return search(arr, firstElement, searchElement, left, middle);
+        }else{             
+            return search(arr, firstElement, searchElement, middle + 1, right);          
+        }
+        
+    }
+
 }
+
+
 
 function brokenSearch(arr, k) {
     let firstElement = Number(arr[0]);
-    let breakIndex = searchMinElement(arr, firstElement, firstElement, 0, arr.length);
-    let result = -1;
-    if(Number(k) <= firstElement){
-        result = search(arr, k, breakIndex, arr.length);
-    }else{
-        result = search(arr, k, 0, breakIndex);
-    }    
+    return search(arr, firstElement, k, 0, arr.length);
+}
 
-    return result;
+function test_assert(arr, find){
+    
+    let result = brokenSearch(arr, find);
+
+    console.log(`${arr} ${find} ${result}`);
+
 }
 
 function test() {
-    let arr = [];
+       
+    test_assert([1, 2, 3, 4, 5, 6, 7, 8, 9], 1);
+    test_assert([1, 2, 3, 4, 5, 6, 7, 8, 9], 2);
+    test_assert([1, 2, 3, 4, 5, 6, 7, 8, 9], 3);
+    test_assert([1, 2, 3, 4, 5, 6, 7, 8, 9], 4);
+    test_assert([1, 2, 3, 4, 5, 6, 7, 8, 9], 5);
+    test_assert([1, 2, 3, 4, 5, 6, 7, 8, 9], 6);
+    test_assert([1, 2, 3, 4, 5, 6, 7, 8, 9], 7);
+    test_assert([1, 2, 3, 4, 5, 6, 7, 8, 9], 8);
+    test_assert([1, 2, 3, 4, 5, 6, 7, 8, 9], 9);
+    test_assert([1, 2, 3, 4, 5, 6, 7, 8, 9], 10, true);
+    test_assert([1, 2, 3, 4, 5, 6, 7, 8, 9], -1, true);
+ 
+    test_assert([3, 4, 5, 6, 7, 8, 9, 1, 2], 3);
+    test_assert([3, 4, 5, 6, 7, 8, 9, 1, 2], 4);
+    test_assert([3, 4, 5, 6, 7, 8, 9, 1, 2], 5);
+    test_assert([3, 4, 5, 6, 7, 8, 9, 1, 2], 6);
+    test_assert([3, 4, 5, 6, 7, 8, 9, 1, 2], 7);
+    test_assert([3, 4, 5, 6, 7, 8, 9, 1, 2], 8);
+    test_assert([3, 4, 5, 6, 7, 8, 9, 1, 2], 9);
+    test_assert([3, 4, 5, 6, 7, 8, 9, 1, 2], 1);
+    test_assert([3, 4, 5, 6, 7, 8, 9, 1, 2], 2);
+    test_assert([3, 4, 5, 6, 7, 8, 9, 1, 2], 121, true);
 
-    arr = [19, 21, 100, 101, 1, 4, 5, 7, 12];
-    if (brokenSearch(arr, 5) !== 6)  {
-        console.error("WA " + arr);
-    }
+    test_assert([7, 8, 1, 2, 3, 4, 5, 6], 7);
+    test_assert([7, 8, 1, 2, 3, 4, 5, 6], 8);
+    test_assert([7, 8, 1, 2, 3, 4, 5, 6], 1);
+    test_assert([7, 8, 1, 2, 3, 4, 5, 6], 2);
+    test_assert([7, 8, 1, 2, 3, 4, 5, 6], 3);
+    test_assert([7, 8, 1, 2, 3, 4, 5, 6], 4);
+    test_assert([7, 8, 1, 2, 3, 4, 5, 6], 5);
+    test_assert([7, 8, 1, 2, 3, 4, 5, 6], 6);
 
-    arr = [5, 1];
-    if (brokenSearch(arr, 1) !== 1)  {
-        console.error("WA " + arr);
-    }
-
-    arr = [1, 2, 3, 5, 6, 7, 9, 0];
-    if (brokenSearch(arr, 3) !== 2)  {
-        console.error("WA " + arr);
-    }
-
-    arr = [1];
-    if (brokenSearch(arr, 1) !== 0)  {
-        console.error("WA " + arr);
-    }
+    test_assert([7, 8, 1, 2, 3, 4, 5, 6], 42, true);    
 }
+
+
 
 //test();
