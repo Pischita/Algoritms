cfcab1126e2cf12a53d5cdd4871df1d8a313f1c0
Ivan Pischita <aaib@tut.by>
2021-12-02 22:56:34 +0300

    2

--- sprint_5/b_delete_node.js
+++ sprint_5/b_delete_node.js
@@ -8,8 +8,11 @@ function getLeaf(node, side='left'){
             currentNode = currentNode.right; 
         }
 
-        console.log(currentNode);
-        console.log(parent);
+        if(parent){
+            parent.right = null;
+        }
+        
+        return currentNode;
     }
 
 }
@@ -29,46 +32,70 @@ function remove(node, key) {
         }
          
         parentForDeleted = currentNode;
-        if( key > node.value){
+        if( key > currentNode.value){
             currentNode = currentNode.right; 
         }else{
             currentNode = currentNode.left;
         }
-
-
     }
 
-    //console.log(deletedNode);
-    //console.log(parentForDeleted);
-    getLeaf(deletedNode);
+    if(deletedNode){
+        if(parentForDeleted){
+            let isLeft = parentForDeleted.left === deletedNode;
+            if(deletedNode.left === null || deletedNode.right === null){
+                if(isLeft){
+                    parentForDeleted.left = deletedNode.left || deletedNode.right;
+                }else{
+                    parentForDeleted.right = deletedNode.left || deletedNode.right;
+                }
+            }else{
+                if(isLeft){
+                    parentForDeleted.left = getLeaf(deletedNode);
+                }else{
+                    parentForDeleted.right = getLeaf(deletedNode);
+                } 
 
-
-
+            }
+        }else{
+            //console.lof('heleted head');
+             node = getLeaf(deletedNode);
+             node.left = deletedNode.left;
+             node.right = deletedNode.right; 
+    
+        }
+    
+    }
+    
     return node;
     
 }
 
-class Node { 
-    constructor(value, left = null, right = null) { 
-        this.value = value; 
-        this.left = left; 
-        this.right = right; 
-    } 
-}
+// class Node { 
+//     constructor(value, left = null, right = null) { 
+//         this.value = value; 
+//         this.left = left; 
+//         this.right = right; 
+//     } 
+// }
 
 
-function test() {
-    var node1 = new Node(2, null, null);
-    var node2 = new Node(3, node1, null);
-    var node3 = new Node(1, null, node2);
-    var node4 = new Node(6, null, null);
-    var node5 = new Node(8, node4, null);
-    var node6 = new Node(10, node5, null);
-    var node7 = new Node(5, node3, node6);
-    var newHead = remove(node7, 10);
-    console.assert(newHead.value === 5);
-    console.assert(newHead.right === node5);
-    console.assert(newHead.right.value === 8);
-}
+// function test() {
+//     var node1 = new Node(2, null, null);
+//     var node2 = new Node(3, node1, null);
+//     var node3 = new Node(1, null, node2);
+//     var node4 = new Node(6, null, null);
+//     var node5 = new Node(8, node4, null);
+//     var node6 = new Node(10, node5, null);
+//     var node7 = new Node(5, node3, node6);
+//     //var newHead = remove(node7, 10);
+//     //console.assert(newHead.value === 5);
+//     //console.assert(newHead.right === node5);
+//     //console.assert(newHead.right.value === 8);
 
-test();
+//     //let node11 = new Node(11);
+//     //node6.right = node11;
+//     var newHead = remove(node7, 5);
+//     console.log(newHead);
+// }
+
+// test();
