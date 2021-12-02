f97ca6464f4e62ff2abfd0a16d950c9d31fa2350
Ivan Pischita <aaib@tut.by>
2021-12-02 23:55:43 +0300

    4

--- sprint_5/b_delete_node.js
+++ sprint_5/b_delete_node.js
@@ -89,38 +89,39 @@ function remove(node, key) {
     
 }
 
-// class Node { 
-//     constructor(value, left = null, right = null) { 
-//         this.value = value; 
-//         this.left = left; 
-//         this.right = right; 
-//     } 
-// }
+class Node { 
+    constructor(value, left = null, right = null) { 
+        this.value = value; 
+        this.left = left; 
+        this.right = right; 
+    } 
+}
 
 
-// function test() {
-//     // var node1 = new Node(2, null, null);
-//     // var node2 = new Node(3, node1, null);
-//     // var node3 = new Node(1, null, node2);
-//     // var node4 = new Node(6, null, null);
-//     // var node5 = new Node(8, node4, null);
-//     // var node6 = new Node(10, node5, null);
-//     // var node7 = new Node(5, node3, node6);
-//     // //var newHead = remove(node7, 10);
-//     // //console.assert(newHead.value === 5);
-//     // //console.assert(newHead.right === node5);
-//     // //console.assert(newHead.right.value === 8);
+function test() {
+    // var node1 = new Node(2, null, null);
+    // var node2 = new Node(3, node1, null);
+    // var node3 = new Node(1, null, node2);
+    // var node4 = new Node(6, null, null);
+    // var node5 = new Node(8, node4, null);
+    // var node6 = new Node(10, node5, null);
+    // var node7 = new Node(5, node3, node6);
+    // //var newHead = remove(node7, 10);
+    // //console.assert(newHead.value === 5);
+    // //console.assert(newHead.right === node5);
+    // //console.assert(newHead.right.value === 8);
 
-//     // let node11 = new Node(11);
-//     // node6.right = node11;
-//     // var newHead = remove(node7, 1);
-//     // console.log(newHead);
+    // let node11 = new Node(11);
+    // node6.right = node11;
+    // var newHead = remove(node7, 1);
+    // console.log(newHead);
 
-//     var node1 = new Node(2, null, null);
-//     var node2 = new Node(3, node1, null);
-//     var newHead = remove(node2, 3);
-//      console.log(newHead);
+    var node1 = new Node(-1, null, null);
+    let node5 = new Node(2, null, null);
+    var node2 = new Node(1, node1, node5);
+    var newHead = remove(node2, 2);
+     console.log(newHead);
     
-// }
+}
 
-// test();
+test();
