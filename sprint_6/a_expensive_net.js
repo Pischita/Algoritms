e6290c7d7bccddd95d42f4317d8c6b8d74fb366d
Ivan Pischita <aaib@tut.by>
2021-12-22 06:56:00 +0300

    1

--- sprint_6/a_expensive_net.js
+++ sprint_6/a_expensive_net.js
@@ -56,13 +56,14 @@ function extractMaximum(v, vertexEdges){
 }
 
 
-function findMST(edges) {
+function findMST(edges, startVertex) {
+    minimum_spanning_tree = [];
     not_added = new Array(edges.length);
     for (let i = 0; i < edges.length; i++) {
         not_added[i] = i;
     }
 
-    let v = not_added[2];
+    let v = not_added[startVertex];
     minimum_spanning_tree.push(v);
     let vertexEdges = add_vertex(v);
     let currentVertex = v;
@@ -77,14 +78,18 @@ function findMST(edges) {
             vertexEdges = add_vertex(v);
             currentVertex = v;
         }
-        
-
     }
 
     if(not_added.length > 0) {
-        console.log('Исходный граф несвязный');
+        return 0;
     } else{
-        console.log(minimum_spanning_tree);
+        let result = 0;
+        for(let i = 1; i < minimum_spanning_tree.length; i++){
+            edgeStart = minimum_spanning_tree[i-1];
+            edgeEnd = minimum_spanning_tree[i];
+            result += edges[edgeStart][edgeEnd];
+        }
+        return result;
     }
 
 }
@@ -104,13 +109,15 @@ function solve() {
     let skipLines = 1;
     for (let i = 0; i < countEdges; i++) {
         let [v1, v2, weight] = inputLines[i + skipLines].split(' ');
+        weight = Number(weight); 
+        if( edges[Number(v1) - 1][Number(v2) - 1] === undefined ||  edges[Number(v1) - 1][Number(v2) - 1]  < weight)
         edges[Number(v1) - 1][Number(v2) - 1] = Number(weight);
         edges[Number(v2) - 1][Number(v1) - 1] = Number(weight);
     }
 
     for (let i = 0; i < edges.length; i++) {
         for (let j = 0; j < edges[i].length; j++) {
-            if (i === j) {
+            if (i === j && edges[i][j] === undefined) {
                 edges[i][j] = 0;
             }
             if (edges[i][j] === undefined) {
@@ -119,17 +126,47 @@ function solve() {
         }
     }
 
-    findMST(edges);
-    //console.log(edges);
+    let maxResult = 0;
+    for(let startVertex = 0; startVertex < countVertex; startVertex++){
+        let result = findMST(edges, startVertex);
+        if(result > maxResult){
+            maxResult = result;
+        }
+    }
+    
+    if(countVertex === 1){
+        console.log('0');
+    }else if(maxResult === 0){
+        console.log('Oops! I did it again'); 
+    }else{
+       console.log(maxResult); 
+    }
+    
 
 }
 
 
-let input = `4 4
-1 2 5
-1 3 6
-2 4 8
-3 4 3
+let input = `10 20
+8 7 9
+4 10 7
+6 6 2
+8 10 1
+10 6 1
+5 7 8
+1 9 6
+10 3 3
+10 5 8
+6 6 6
+5 7 9
+5 2 4
+3 1 1
+10 7 8
+8 4 6
+5 5 7
+7 8 6
+5 10 2
+10 1 3
+3 5 9
 `;
 
 inputLines = input.split('\n');
