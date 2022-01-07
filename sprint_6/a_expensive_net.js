26da8abd6ddab9c8a53f1831c21179f65fca35f2
Ivan Pischita <aaib@tut.by>
2022-01-07 19:59:04 +0300

    Ok

--- sprint_6/a_expensive_net.js
+++ sprint_6/a_expensive_net.js
@@ -15,84 +15,75 @@ _reader.on('line', line => {
 process.stdin.on('end', solve);
 
 let edges = [];
-let minimum_spanning_tree = [] //# Рёбра, составляющие MST.
+let spanning_tree = [] //# Рёбра, составляющие MST.
 
 let added = [] // # Множество вершин, уже добавленных в остов.
 let not_added = [] //  # Множество вершины, ещё не добавленных в остов.  
 
-function add_vertex(v) {
-    added.push(v);
-
-    not_added = not_added.filter(vertex => vertex != v);
-
-    let vertexEdges =[] 
-    edges[v].forEach( (weight, i) => {
-        if(weight != 0){
-            vertexEdges.push(i);
-        }    
-    });
-    return vertexEdges;
-}
-
-function extractMaximum(v, vertexEdges){
-    let maxWeight = 0;
-    let maxVertex;
-    vertexEdges.forEach((i) =>{    
-        if(maxWeight < edges[v][i]){
-            maxWeight = edges[v][i];
-            maxVertex = i;
+class Edge{
+    constructor(v1, v2, weight){
+        if(v1 < v2){
+            this.start = v1; 
+            this.end = v2;
+        }else{
+            this.start = v2; 
+            this.end = v1;
         }
-    });
-
-    for(let i = 0; i< vertexEdges.length; i++){
-        if(vertexEdges[i] === maxVertex){
-            vertexEdges.splice(i, 1);
-            break;
-        }
-    }    
-
-    return maxVertex;
-
+        
+        this.weight = weight;
+    }
 }
 
 
-function findMST(edges, startVertex) {
-    minimum_spanning_tree = [];
-    not_added = new Array(edges.length);
-    for (let i = 0; i < edges.length; i++) {
-        not_added[i] = i;
-    }
 
-    
-    
+function findMST() {
+    spanning_tree = [];
+    // not_added = new Array(edges.length);
+    // for (let i = 0; i < edges.length; i++) {
+    //     not_added[i] = i;
+    // }
     
 
-    minimum_spanning_tree.push(not_added[0]);
-    let vertexEdges = [];
-
-    while(not_added.length v> 0){
-        let currentVertex = not_added[0];        
-        vertexEdges = add_vertex(currentVertex);
-        v = extractMaximum(currentVertex, vertexEdges);
-
-        let isNotVisited = not_added.findIndex( vertex => vertex === v);
-        if(isNotVisited != -1){
-            minimum_spanning_tree.push(v);
+    for(let i = 0; i < edges.length; i++){
+        edge = edges[i];
+        if (!isLoop(edge)){
+            spanning_tree.push(edge);
         }
-    }
 
-    if(not_added.length > 0) {
-        return 0;
-    } else{
-        let result = 0;
-        for(let i = 1; i < minimum_spanning_tree.length; i++){
-            edgeStart = minimum_spanning_tree[i-1];
-            edgeEnd = minimum_spanning_tree[i];
-            result += edges[edgeStart][edgeEnd];
-        }
+    }
+}
+
+function isLoop(edge){
+    let result;
+
+    if(edge.start === edge.end){
+        result = true;
         return result;
     }
 
+    let colorStart = added[edge.start];
+    let colorEnd = added[edge.end];
+    if(colorStart === 0 && colorEnd === 0){
+        result = false;
+        added[edge.start] = edge.start;
+        added[edge.end] = added[edge.start];
+    }else if(colorStart === colorEnd){
+        result = true;
+    }else if(colorStart != colorEnd){
+        result = false;
+        if(colorStart === 0){
+            added[edge.start] = added[edge.end];
+        }else if(colorEnd === 0){
+            added[edge.end] = added[edge.start];
+        }else {
+           for(let i = 0; i < added.length; i++){
+               if(added[i] === colorEnd){
+                   added[i] = colorStart;
+               }
+           } 
+        }
+    }
+    return result;
 }
 
 
@@ -102,71 +93,61 @@ function solve() {
     const countVertex = Number(graphData[0]);
     const countEdges = Number(graphData[1]);
 
-    edges = new Array(countVertex);
-    for (let i = 0; i < countVertex; i++) {
-        edges[i] = new Array(countVertex);
-    }
-
+    
     let skipLines = 1;
     for (let i = 0; i < countEdges; i++) {
         let [v1, v2, weight] = inputLines[i + skipLines].split(' ');
-        weight = Number(weight); 
-        if( edges[Number(v1) - 1][Number(v2) - 1] === undefined ||  edges[Number(v1) - 1][Number(v2) - 1]  < weight)
-        edges[Number(v1) - 1][Number(v2) - 1] = Number(weight);
-        edges[Number(v2) - 1][Number(v1) - 1] = Number(weight);
+        edges.push( new Edge(Number(v1), Number(v2), Number(weight)) );        
     }
+    added = new Array(countVertex + 1).fill(0);
 
-    for (let i = 0; i < edges.length; i++) {
-        for (let j = 0; j < edges[i].length; j++) {
-            if (i === j && edges[i][j] === undefined) {
-                edges[i][j] = 0;
-            }
-            if (edges[i][j] === undefined) {
-                edges[i][j] = 0;
-            }
-        }
-    }
+    edges.sort((e1, e2) => e2.weight - e1.weight);
+    findMST();
 
-    let maxResult = 0;
-    for(let startVertex = 0; startVertex < countVertex; startVertex++){
-        let result = findMST(edges, startVertex);
-        if(result > maxResult){
-            maxResult = result;
-        }
-    }
+    let result = spanning_tree.reduce((prevValue, item)=>{
+        return prevValue += item.weight;
+    }, 0)
     
-    if(countVertex === 1){
-        console.log('0');
-    }else if(maxResult === 0){
-        console.log('Oops! I did it again'); 
-    }else{
-       console.log(maxResult); 
+    //console.log(spanning_tree);
+    if (result === 0 && countVertex > 1){
+        result = 'Oops! I did it again';
+    }else if(added.length > 1){
+        // Проверим что граф связанный
+        let color = added[1];
+        for(let i = 2; i < added.length; i++){
+            if (color != added[i]){
+                result = 'Oops! I did it again';
+                break; 
+            }
+        }
     }
+    console.log(result);
+
+   
 }
 
 
 let input = `10 20
-9 10 4
-2 2 4
-4 2 8
-10 5 3
-1 10 6
-7 4 2
-10 10 6
-3 7 4
-8 9 4
-8 10 7
-6 10 10
-2 8 8
-3 8 1
+2 8 6
+7 2 1
+2 9 4
+5 4 9
+8 9 1
+7 10 3
+10 10 8
+9 2 5
+4 3 5
+1 10 5
+6 2 10
+2 6 10
+8 2 8
+9 2 3
+10 2 1
 3 10 3
-9 5 8
-10 10 2
-1 8 1
-10 1 5
-3 6 10
-9 10 8
-
+2 8 5
+2 8 4
+7 1 1
+7 5 4
 `;
 
 inputLines = input.split('\n');
