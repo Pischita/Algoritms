ffa2d24121d88b7dbadf42e47e61da666eba5541
Ivan <aaib@tut.by>
2022-02-06 13:06:47 +0300

    Без использования подстроки

--- sprint_8/a_packed_prefix.js
+++ sprint_8/a_packed_prefix.js
@@ -35,10 +35,10 @@ function prefixFunction(str, firstPosition = 1) {
 
 }
 
-function decode(str){
+function decode(str, startIndex = 0){
     let result = '';
 
-    let i = 0;
+    let i = startIndex;
     let multiplier = 0;
     while(i < str.length){
         let currentSymbal = str[i];
@@ -48,15 +48,12 @@ function decode(str){
         if(isNumber){
             multiplier = Number(currentSymbal);
         }else if(currentSymbal === '['){
-            let expression = decode(str.substring(i+1));
+            let expression = decode(str, i+1);
             while(multiplier > 0){
                 result += expression.text;
                 multiplier--;
             }
-            i += expression.lastIndex + 1;
-            // while(str[i+1] === ']'){
-            //     i++;
-            // }
+            i = expression.lastIndex;
         }else if(currentSymbal === ']'){
             break;
         }else{
@@ -69,66 +66,6 @@ function decode(str){
     return {text:result, lastIndex:i};
 }
 
-function decodeString(str) {
-
-    let result = [];
-
-    let i = 0;
-    let subStr = '';
-    while (i < str.length) {
-        let currentSymbol = str[i];
-
-        let isNumber = Number(str[i]) >= 0 && Number(str[i]) <= 9;
-
-        if (isNumber) {
-            if (subStr) {
-                result.push(subStr);
-                subStr = '';
-            }
-            result.push(str[i]);
-        } else if (currentSymbol === ']') {
-            if (subStr) {
-                result.push(subStr);
-                subStr = '';
-            }
-            result.push('+');
-        } else if (currentSymbol === '[') {
-            result.push('*');
-        } else {
-            subStr += currentSymbol;
-        }
-        i++;
-    }
-
-
-    let resultString = '';
-    let sign = '';
-    subStr = ''
-    for(let i = result.length -1; i >= 0; i--){
-        let isNumber = Number(result[i]) >= 0 && Number(result[i]) <= 9;
-
-        if(isNumber){
-            let multiplier = Number(result[i] );
-            while(multiplier >= 1){
-                resultString = subStr + resultString;
-                multiplier--;
-            }
-        }else if(result[i] ==='+'){
-            
-        }else if(result[i]==='*') {
-            sign = result[i];
-        }else{
-            subStr = result[i];
-        }
-
-    }
-
-    console.log(result);
-    console.log(resultString);
-
-    return resultString;
-
-}
 
 function prefix(prefixVector, str){
     let result = '';
@@ -166,21 +103,17 @@ function solve() {
     for(let i = 1; i < strings.length; i++){
         let str = currentPrefix + '#' + strings[i]
         let pfunc = prefixFunction(str, currentPrefix.length + 1);
-        currentPrefix = prefix(pfunc, str)
-
+        currentPrefix = prefix(pfunc, str);
     }
 
     console.log(currentPrefix);
 }
 
 
-let input = `5
-kedkedkedbjbjrcrcrcjvmjvmorchorchorchcjcjcjkedkedkedbjbjrcrcrcjvmjvmorchorchorchcjcjcjejkejkejkyqkwyqkwyqkwxvxvxvmhcomhcornfpmbbbkhjqqxqxmhcomhcornfpmbbbkhjqqxqxmhcomhcornfpmbbbkhjqqxqx3[liw]
-kedkedkedbjbjrcrcrcjvmjvmorchorchorchcjcjcjkedkedkedbjbjrcrcrcjvmjvmorchorchorchcjcjcjejkejkejkyqkwyqkwyqkwxvxvxvmhcomhcornfpmbbbkhjqqxqxmhcomhcornfpmbbbkhjqqxqxmhcomhcornfpmbbbkhjqqxqx1[fseg]
-2[3[ked]2[bj]3[rc]2[jvm]3[orch]3[cj]]3[ejk]1[3[yqkw]3[xv]]3[2[mhco]1[rn]1[fpm]3[b]1[khjq]2[qx]]2[zfxa]
-kedkedkedbjbjrcrcrcjvmjvmorchorchorchcjcjcjkedkedkedbjbjrcrcrcjvmjvmorchorchorchcjcjcjejkejkejkyqkwyqkwyqkwxvxvxvmhcomhcornfpmbbbkhjqqxqxmhcomhcornfpmbbbkhjqqxqxmhcomhcornfpmbbbkhjqqxqx2[t]
-kedkedkedbjbjrcrcrcjvmjvmorchorchorchcjcjcjkedkedkedbjbjrcrcrcjvmjvmorchorchorchcjcjcjejkejkejkyqkwyqkwyqkwxvxvxvmhcomhcornfpmbbbkhjqqxqxmhcomhcornfpmbbbkhjqqxqxmhcomhcornfpmbbbkhjqqxqx2[3[fdt]]1[3[vv]1[cg]3[grxm]]
-`;
+let input = `3
+2[a]2[ab]
+3[a]2[r2[t]]
+a2[aa3[b]]`;
 
 inputLines = input.split('\n');
 
